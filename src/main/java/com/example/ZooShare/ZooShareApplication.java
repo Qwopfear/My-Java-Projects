781c107d5c896be0874c1362d7c5191d0bf1f93f
Диана Берашевич <dianaberasevich@macbook-pro-diana.home>
2021-12-23 22:57:02 +0100

    Start of the project. The database is connected, the main classes are described.

---
+++ src/main/java/com/example/ZooShare/ZooShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZooShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZooShare/ZooShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZooShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
