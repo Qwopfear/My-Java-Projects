8f4a4fb74bb098e0a79988357cdd8c508fa70756
Qwopfear <dianaberasevich@macbook-pro-diana.home>
2022-01-03 15:30:02 +0100

    Test 1

--- src/main/java/com/example/ZooShare/ZooShareApplication.java
+++ src/main/java/com/example/ZooShare/ZooShareApplication.java
@@ -9,7 +9,7 @@ public class ZooShareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZooShareApplication.class, args);
 		System.out.println("Hello!");
-		System.out.println("Spring seq");
+		System.out.println("On project works only one person");
 	}
 
 }
